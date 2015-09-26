e9ea637ce60f1951d92883d0d9ffd2048e9859a5
LinaDovhaniuk <lina.dovhaniuk@gmail.com>
2015-09-26 15:51:28 +0300

    Self

---
+++ courses/prog_base/self/26.09.c
@@ -0,0 +1,16 @@
+int main() {
+	int code, h, m;
+	float price;
+		scanf("%i %i %f", &code, &h, &m);
+		switch (code){
+			case 44: price=0.44*(h*60+m);
+					break;
+			case 666: price=0*(h*60+m);
+					break;
+			case 1: price=30*(h*60+m);
+					printf("Price %f", price);
+					break;
+		
+		}
+		return 0;
+} 
