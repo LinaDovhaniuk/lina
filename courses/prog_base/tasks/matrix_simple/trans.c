28b998e96fd699d6e38c09b240dfa7f105f9754b
LinaDovhaniuk <lina.dovhaniuk@gmail.com>
2015-11-14 08:33:15 +0200

    Matrix_Simple

--- courses/prog_base/tasks/matrix_simple/trans.c
+++ courses/prog_base/tasks/matrix_simple/trans.c
@@ -1,4 +1,4 @@
-/*#include <stdlib.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 
@@ -13,6 +13,7 @@ int main ()
 {
 	int i,j;
 	int mat [4][4];
+    srand(time(NULL));
 	fillRand(mat);
 	printMatrix(mat);
 	rotateCW90(mat);
@@ -25,17 +26,16 @@ int main ()
 	system ("pause");
 	return(0);
 }
-*/
-void fillRand(int mat[4][4]){
-srand (time(NULL));
-int i, j, n = 4;
-    for (i = 0; i < n; i++){
-        for (j = 0; j < n; j++){
-            mat[i][j] = ((int)rand() % 1999) - 999;
-    }
-    }
-}
 
+void fillRand(int mat [4][4]){
+    int i,j;
+
+	for (i = 0; i < 4; i++){
+		for (j = 0; j < 4; j++){
+			mat[i][j] =rand()%1999-999;
+		}
+	}
+}
 void printMatrix(int mat[4][4]) {
 int i, j;
 int n = 4;
