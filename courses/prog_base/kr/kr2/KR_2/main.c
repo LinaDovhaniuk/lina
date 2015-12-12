a0ff016e0441b4d243f9cc7cb41a4eda649f9e95
LinaDovhaniuk <lina.dovhaniuk@gmail.com>
2015-12-12 12:36:12 +0200

    KR_2

---
+++ courses/prog_base/kr/kr2/KR_2/main.c
@@ -0,0 +1,72 @@
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+void length(char tmp, char * p1);
+int main(){
+
+    char * fileName = "input.txt";
+    char * outPut = "output.txt";
+    FILE * fp;
+    char tmp;
+    int status = 0;
+    int count = 1;
+    char str[100];
+
+    char *p1;
+    char * p2;
+
+    fp = fopen(fileName, "r");
+    if (NULL == fp && status != 1) {
+        puts("Error!");
+        status = 1;
+        return EXIT_FAILURE;
+    }
+
+    tmp = fgetc(fp);
+    char tmp1 = tmp;
+    while (EOF != tmp && status!=1) {
+        printf("%c", tmp);
+        tmp = fgetc(fp);
+
+    }
+
+
+length(tmp,p1);
+printf("%s",p2);
+
+fclose(fp);
+
+fp = fopen("output.txt", "w");
+    if (fp == NULL) {
+        puts("Error opening output.txt!");
+        return EXIT_FAILURE;
+    }
+
+    fprintf(fp, "%c", p2);
+
+    fclose(fp);
+    return 0;
+
+
+}
+
+void length(char tmp, char * p1){
+int len;
+char * p2;
+p1=strtok(tmp," ");
+ while (p1!=NULL)
+     {
+      len=strlen(p1);
+
+           p2=p1;
+          }
+      p1=strtok(NULL," ");
+
+printf("%c", p2);
+}
+
+
---
+++ courses/prog_base/kr/kr2/task_2.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+
+struct dragon{
+    char name[100];
+    int sizeDr;
+    int age;
+};
+struct Film{
+    char fm[100];
+   // struct dragon d;
+    int date;
+};
+
+void count(int size, struct Film f[]){
+    //t = 1449394733 06.12.15
+    time_t end;
+    int count = 1;
+    end = time(NULL);
+    int i;
+    int result;
+    for (i = 0; i < 3; i++){
+        printf("%d. %s\n", i+1, f[i].fm);
+        printf("Date of release %d \n", f[i].date);
+        result = difftime(end, f[i].date);
+        if (result < (end - 818763558) ){
+            count++;
+        }
+    }
+	printf("The number of films (<1995)%i", count);
+}
+
+void print (int size,struct dragon * dr ){
+    int i;
+
+    for (i = 0; i < 3; i++){
+
+        printf("%d. %s\n", i + 1, dr[i].name);
+        printf("Size : %i", dr[i].sizeDr);
+        printf("  Age  : %d \n", dr[i].age);
+
+    }
+}
+
+void pr(int size,struct dragon * dr ){
+    int i;
+
+    for (i = 0; i < 3; i++){
+        if (dr[i].sizeDr>20){
+        printf("%d. %s\n", i + 1, dr[i].name);
+        printf("Size : %i", dr[i].sizeDr);
+        printf("  Age  : %d \n", dr[i].age);
+        }
+    }
+}
+
+int main(){
+    printf("Actors :\n");
+    struct dragon test;
+    struct dragon dr[3] = {"First", 15,150,"Second", 30, 200,"Third", 40, 170};
+    int size = sizeof(dr) / (sizeof(test));
+
+    print(size, dr);
+
+    printf("\nThe correct actors :\n");
+    pr(size,dr);
+
+    printf("\nFilms :\n");
+    struct Film  f[3] = {"Dragons", 2010,"Arrr", 2000, "Blabla", 1900};
+    count(size,f);
+
+
+}
