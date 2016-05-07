4d1ecd566e9b595fb7000141d0980743f128fae6
LinaDovhaniuk <lina.dovhaniuk@gmail.com>
2016-05-07 21:40:10 +0300

    CoachWidget

---
+++ courses/prog_base_3/project/AddCoach/mainwindow.cpp
@@ -0,0 +1,105 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setWindowTitle("QDataWidgetMapper Example");
+    /* Первым делом необходимо создать объект для работы с базой данных
+     * и инициализировать подключение к базе данных
+     * */
+    db = new DataBase();
+    db->connectToDataBase();
+
+    /* Инициализируем модели для представления данных
+     * с заданием названий колонок
+     * */
+    this->setupModel(COACH,
+                     QStringList() << trUtf8("id")
+                                         << trUtf8("Фамилия")
+                                         << trUtf8("Имя")
+                                         << trUtf8("Дата рождения")
+                                         << trUtf8("Телефон")
+               );
+    /* Инициализируем внешний вид таблицы с данными
+     * */
+    this->createUI();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::setupModel(const QString &tableName, const QStringList &headers)
+{
+    /* Производим инициализацию модели представления данных
+     * */
+    modelDevice = new QSqlTableModel(this);
+    modelDevice->setTable(tableName);
+    modelDevice->select();
+    /* Устанавливаем названия колонок в таблице с сортировкой данных
+     * */
+    for(int i = 0, j = 0; i < modelDevice->columnCount(); i++, j++){
+        modelDevice->setHeaderData(i,Qt::Horizontal,headers[j]);
+    }
+}
+
+void MainWindow::createUI()
+{
+    ui->deviceTableView->setModel(modelDevice);     // Устанавливаем модель на TableView
+    ui->deviceTableView->setColumnHidden(0, true);    // Скрываем колонку с id записей
+    // Разрешаем выделение строк
+    ui->deviceTableView->setSelectionBehavior(QAbstractItemView::SelectRows);
+    // Устанавливаем режим выделения лишь одно строки в таблице
+    ui->deviceTableView->setSelectionMode(QAbstractItemView::SingleSelection);
+    // Устанавливаем размер колонок по содержимому
+    ui->deviceTableView->resizeColumnsToContents();
+    ui->deviceTableView->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    ui->deviceTableView->horizontalHeader()->setStretchLastSection(true);
+
+    connect(ui->deviceTableView, SIGNAL(doubleClicked(QModelIndex)), this, SLOT(slotEditRecord(QModelIndex)));
+}
+
+/* Метод для активации диалога добавления записей
+ * */
+void MainWindow::on_addDeviceButton_clicked()
+{
+    /* Создаем диалог и подключаем его сигнал завершения работы
+     * к слоту обновления вида модели представления данных
+     * */
+    DialogAddDevice *addDeviceDialog = new DialogAddDevice();
+    connect(addDeviceDialog, SIGNAL(signalReady()), this, SLOT(slotUpdateModels()));
+
+    /* Выполняем запуск диалогового окна
+     * */
+    addDeviceDialog->setWindowTitle(trUtf8("Добавить тренера"));
+    addDeviceDialog->exec();
+}
+
+/* Слот обновления модели представления данных
+ * */
+void MainWindow::slotUpdateModels()
+{
+    modelDevice->select();
+}
+
+/* Метод для активации диалога добавления записей в режиме редактирования
+ * с передачей индекса выбранной строки
+ * */
+void MainWindow::slotEditRecord(QModelIndex index)
+{
+    /* Также создаем диалог и подключаем его сигнал завершения работы
+     * к слоту обновления вида модели представления данных, но передаём
+     * в качестве параметров строку записи
+     * */
+    DialogAddDevice *addDeviceDialog = new DialogAddDevice(index.row());
+    connect(addDeviceDialog, SIGNAL(signalReady()), this, SLOT(slotUpdateModels()));
+
+    /* Выполняем запуск диалогового окна
+     * */
+    addDeviceDialog->setWindowTitle(trUtf8("Редактировать данные"));
+    addDeviceDialog->exec();
+}
---
+++ courses/prog_base_3/project/QDataMapperWidget/QDataMapperWidget.pro
@@ -0,0 +1,27 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-08-16T23:58:29
+#
+#-------------------------------------------------
+
+QT       += core
+QT       += gui
+QT       += sql
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = QDataMapperWidget
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp \
+    database.cpp \
+    dialogadddevice.cpp
+
+HEADERS  += mainwindow.h \
+    database.h \
+    dialogadddevice.h
+
+FORMS    += mainwindow.ui \
+    dialogadddevice.ui
